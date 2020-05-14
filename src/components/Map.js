af14d79dabcd12b1901a6db9807cdb4e6df1a7fd
conbec <56614504+conbec@users.noreply.github.com>
2020-05-13 18:11:09 -0700

    coords and fly working

--- src/components/Map.js
+++ src/components/Map.js
@@ -2,15 +2,23 @@ import React, { useState, useEffect } from 'react';
 import tt from '@tomtom-international/web-sdk-maps';
 
 const Map = () => {
-  let position;
   let map;
 
-  useEffect(() => {
-    navigator.geolocation.getCurrentPosition(_position => {
-      position = _position;
-      console.log(position)
+  const centerMap = (position) => {
+    map.flyTo({
+      center: {
+        lng: position.coords.longitude,
+        lat: position.coords.latitude
+      },
+      zoom: 14 // you can also specify zoom level
+    })
+  }
+
+  const getCoords = () => {
+    navigator.geolocation.getCurrentPosition((position) => {
+      centerMap(position);
     });
-  }, [])
+  }
 
   useEffect(() => {
     map = tt.map({
@@ -18,27 +26,12 @@ const Map = () => {
       container: 'map',
       style: 'tomtom://vector/1/basic-main'
     });
-
     map.addControl(new tt.FullscreenControl())
     map.addControl(new tt.NavigationControl())
-  }, [position]);
+  }, []);
 
   useEffect(() => {
-    /*
-    navigator.geolocation.getCurrentPosition(_position => {
-      position = _position;
-      console.log(position)
-    });
-    */
-    map.on('load', () => {
-      map.flyTo({
-        center: {
-          lng: position.coords.longitude,
-          lat: position.coords.latitude
-        },
-        zoom: 14 // you can also specify zoom level
-      })
-    })
+    getCoords()
   }, [map]);
 
   return (
