15813a2662de07d97a9bf63c9e57ba14a31c6827
conbec <56614504+conbec@users.noreply.github.com>
2020-05-14 19:22:16 -0700

    added home page, did some styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Route, Link, useHistory } from 'react-router-dom';
 import Map from './Map';
+import Home from './Home';
 
 const App = () => {
+  const history = useHistory();
   return (
     <div id="app-container">
-      <Map />
+      <Route exact path="/" render={() => <Home history={history} />} />
+      <Route path="/map" render={() => <Map />} />
     </div>
   )
 }
