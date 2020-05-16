e02e66273a84401b903223bddd49ca54390f6804
conbec <56614504+conbec@users.noreply.github.com>
2020-05-15 20:47:30 -0700

    changing map works, fly works

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { useEffect, useState } from 'react';
+import { modes } from '../constants';
 
-const Home = ({ history }) => {
+const Home = ({ history, goToMap }) => {
+  
   return (
     <div id="home-container">
       <h1>Nav Bug</h1>
       <img id="ladybug-img" src="../../assets/ladybug.jpg" />
       <div className="home-navbar">
-        <button type="button" onClick={() => history.push('/map')}>Load Map</button>
+        <button type="button" onClick={() => goToMap(modes.V_BASIC_MAIN)}>Street Map</button>
+        <button type="button" onClick={() => goToMap(modes.V_BASIC_NIGHT)}>Dark Street Map</button>
         <button type="button" onClick={() => history.push('/directions')}>Directions</button>
       </div>
     </div>
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,47 +1,66 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import tt from '@tomtom-international/web-sdk-maps';
+import { modes } from '../constants';
 
-const Map = () => {
+const Map = ({ history }) => {
   let map;
-  const createMap = () => {
+  const mapElementRef = useRef();
+
+  const createMap = (position, mode) => {
     map = tt.map({
       key: 'lhdNJtemDRfjctoDTw5DqAYs2qr9uloY',
       container: 'map',
-      //style: 'tomtom://vector/1/basic-main',
+      style: mode,
       basePath: 'sdk',
       source: 'vector',
     });
     map.addControl(new tt.FullscreenControl())
     map.addControl(new tt.NavigationControl())
+    const { coords } = position;
+    map.on('load', () => {
+      map.flyTo({
+        center: {
+          lat: coords.latitude,
+          lng: coords.longitude
+        },
+        zoom: 14, // you can also specify zoom level
+      })
+    })
   }
 
   const getCoords = () => {
     const options = {
       enableHighAccuracy: true,
       timeout: 5000,
-      maximumAge: 0
+      maximumAge: 3000
     };
     return new Promise(function (resolve, reject) {
       navigator.geolocation.getCurrentPosition(resolve, reject, options);
     });
   }
 
-  const fly = (position) => {
-    const { coords } = position;
-    map.setCenter([coords.longitude, coords.latitude]);
-    map.setZoom(14);
+  const changeMode = async (style) => {
+    map = null;
+    mapElementRef.current.innerHTML = '';
+    console.log(mapElementRef)
+    const position = await getCoords();
+    createMap(position, style)
   }
 
   useEffect(() => {
-    createMap();
     getCoords()
-      .then(pos => fly(pos));
+      .then(pos => createMap(pos));
   }, []);
 
   return (
     <div id="map-copntainer">
       {!map && <h2>Fetching Map</h2>}
-      <div id="map" />
+      <div ref={mapElementRef} id="map" />
+      <div className="map-navbar">
+        <button type="button" onClick={() => changeMode(modes.V_BASIC_MAIN)}>Street Map</button>
+        <button type="button" onClick={() => changeMode(modes.V_BASIC_NIGHT)}>Dark Street Map</button>
+        <button type="button" onClick={() => history.push('/directions')}>Directions</button>
+      </div>
     </div>
   )
 }
